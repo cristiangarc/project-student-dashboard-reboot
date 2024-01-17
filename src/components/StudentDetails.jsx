da2e2cf53ad3a8bbf1158dc6e3e715e926fcd980
juligarc91 <julissagarcia@pursuit.org>
2024-01-17 13:21:10 -0500

    Created static template for Student Details

--- src/components/StudentDetails.jsx
+++ src/components/StudentDetails.jsx
@@ -1,30 +1,78 @@
-const StudentDetails = () => {
-    const handleSubmit = () => {};
-    const handleTextChange = () => {};
+// import React, { useState } from "react";
+
+const StudentDetails = () => { // pass prop for student data
+//   const [formInput, setFormInput] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // form submission stuff here
+    console.log("Form submitted with:", 
+    // formInput
+    );
+  };
+
+  const handleTextChange = (event) => {
+    // setFormInput();
+  };
+
   return (
     <section>
+        {/* will have to map for each section to extract data and have it render on page */}
+      {/* {students.map((student, index) => ( */}
+        <div key="">
+            <img></img>
+          <ul>
+            <li>Name: {"preferredName"} {"middleName"} {"surname"}</li>
+            <li>Email: </li>
+            <li>Date of Birth:</li>
+            <li>Ontrack? or Offtrack?</li>
+          </ul>
+        </div>
+      {/* ))} */}
+    <section/>
+    <section>
+    <div>
+        <p>Codewars</p>
         <ul>
-            <li></li>
+            <li>Current Total: 1804</li>
+            <li>Last Week: 144</li>
+            <li>Goal: 850</li>
+            <li>Percent of Goal Achieved: </li>
+            <li>Cohort: Winter2025</li>
         </ul>
+    </div>
+    <div>
+        <p>Scores</p>
         <ul>
-            <li>
-
-            </li>
+            <li>Assignments</li>
+            <li>Projects</li>
+            <li>Assessments</li>
         </ul>
+    </div>
+    <div>
+        <p>Certifications</p>
         <ul>
-            <li>
-
-            </li>
+            <li>Resume</li>
+            <li>LinkedIn</li>
+            <li>Mock Interview</li>
+            <li>Github</li>
         </ul>
-        <form onSubmit={handleSubmit}>
-            <label>
-                <input onChange={handleTextChange}/>
-                <input onChange={handleTextChange}/>
-                <button>Submit</button>
-            </label>
-        </form>
+    </div>
     </section>
-  )
-}
 
-export default StudentDetails
+      <form onSubmit={handleSubmit}>
+        <label>
+          Author:
+          <input name="Author" onChange={handleTextChange} />
+        </label>
+        <label>
+          Comment:
+          <input name="Comment" onChange={handleTextChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default StudentDetails;
