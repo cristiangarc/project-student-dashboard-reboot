859fe9cf58f446171bf0f9b7d73bf287a375e34c
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-17 11:27:18 -0500

    Added an about page and render that component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import About from "./About";
 
 function App() {
-  return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Student Dashboard</h1>
+            <About />
+        </div>
+    );
 }
 
 export default App;
