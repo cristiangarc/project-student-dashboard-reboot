f915b3d4dfe25a8be1b37d9a4454e90af30cb110
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-17 15:05:57 -0500

    fixed a bug for the function getAllStudents()

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import Aside from "./components/Aside";
 import About from "./components/About";
 import { Routes, Route, Link } from "react-router-dom";
 import StudentDetails from "./components/StudentDetails";
+import { useState } from "react";
 
 function App() {
+    const [students, setStudents] = useState([]);
+
     return (
         <div>
             <h1>Student Dashboard</h1>
             <Link to={"/"}>Home </Link>
             <Link to={"/about"}>About</Link>
             <Aside />
-            <StudentDetails students={students}/>
+            <StudentDetails students={students} />
             <Routes>
                 <Route path="/about" element={<About />}></Route>
+                <Route
+                    path="/:id/student"
+                    element={<StudentDetails student={students[0]} />}
+                />
             </Routes>
         </div>
     );
