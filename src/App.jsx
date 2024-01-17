26c5db2f7c6032cc7a0a9445efd2a227774e343c
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-17 12:52:37 -0500

    updated the github links for the team members, and added a CSS file for the About page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     return (
         <div>
             <h1>Student Dashboard</h1>
+            <Link to={"/"}>Home </Link>
             <Link to={"/about"}>About</Link>
             <Routes>
                 <Route path="/about" element={<About />}></Route>
