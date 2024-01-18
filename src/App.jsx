f742ce7f136b7f920ddf22c119d2d461b1c82816
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-18 14:53:38 -0500

    added a flex container to the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,23 @@ import { useEffect, useState } from "react";
 import students from "./data/data.json";
 import { getAllStudents } from "./components/api.js";
 
+import "./App.css";
+
 function App() {
     const [filteredStudents, setFilteredStudents] = useState([]);
     const determineTrackStatusStudentDetails = (students) => {
-        return students.map(student => {
-            const isOnTrack = student.certifications.resume &&
-                              student.certifications.linkedin &&
-                              student.certifications.github &&
-                              student.certifications.mockInterview &&
-                              student.codewars.current.total > 850;
-        
-            return isOnTrack ? 'On Track' : 'Off Track';
+        return students.map((student) => {
+            const isOnTrack =
+                student.certifications.resume &&
+                student.certifications.linkedin &&
+                student.certifications.github &&
+                student.certifications.mockInterview &&
+                student.codewars.current.total > 850;
+
+            return isOnTrack ? "On Track" : "Off Track";
         });
-    }
-    
+    };
+
     const filterStudents = (cohort) => {
         const cohortNoSpace = cohort.replace(" ", "");
         if (cohortNoSpace === "all") {
@@ -43,23 +46,27 @@ function App() {
             <h1>Student Dashboard</h1>
             <Link to={"/"}>Home </Link>
             <Link to={"/about"}>About</Link>
-            <Aside students={students} filterStudents={filterStudents} />
-            <Routes>
-                <Route
-                    path="/"
-                    element={<AllStudents students={filteredStudents} />}
-                ></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route 
-                    path="/:id/student" 
-                    element={
-                    <StudentDetails 
-                        students={students} 
-                        determineTrackStatusStudentDetails={determineTrackStatusStudentDetails} 
+            <section className="container">
+                <Aside students={students} filterStudents={filterStudents} />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<AllStudents students={filteredStudents} />}
+                    ></Route>
+                    <Route path="/about" element={<About />}></Route>
+                    <Route
+                        path="/:id/student"
+                        element={
+                            <StudentDetails
+                                students={students}
+                                determineTrackStatusStudentDetails={
+                                    determineTrackStatusStudentDetails
+                                }
+                            />
+                        }
                     />
-                } 
-                />
-            </Routes>
+                </Routes>
+            </section>
         </div>
     );
 }
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -29,7 +29,7 @@ const Aside = ({ students, filterStudents }) => {
     allCohorts = standardiseCohorts();
 
     return (
-        <div>
+        <aside>
             Choose a Class by Start Date
             <ul>
                 <li key={v4()} onClick={() => filterStudents("all")}>
@@ -41,7 +41,7 @@ const Aside = ({ students, filterStudents }) => {
                     </li>
                 ))}
             </ul>
-        </div>
+        </aside>
     );
 };
 
