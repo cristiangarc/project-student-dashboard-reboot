27ca2711f25bf986aeae4e65b68527e468aa816e
juligarc91 <julissagarcia@pursuit.org>
2024-01-18 01:01:19 -0500

    added handler fx for on track off track status

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
+
 const StudentDetails = ({ students }) => {
+
+  function determineTrackStatus(student) {
+    const isOnTrack = student.certifications.resume &&
+                      student.certifications.linkedin &&
+                      student.certifications.github &&
+                      student.certifications.mockInterview &&
+                      student.codewars.current.total > 850;
+
+    return isOnTrack ? 'On Track' : 'Off Track';
+  }
 
   return (
     <section>
       {students.map((student) => (
         <Link to={`/${student.id}/student`} key={student.id}>
-        <div key={student.id}>
-          <img src={student.profilePhoto} alt={`${student.names.preferredName}'s Profile`} />
-          <ul>
-            <li>Name: {student.names.preferredName} {student.names.middleName} {student.names.surname}</li>
-            <li>Email: {student.username}</li>
-            <li>Date of Birth: {student.dob}</li>
-            {/* boolean stuff to determine which one for next line */}
-            <li>Ontrack? or Offtrack?</li>
-          </ul>
-        </div>
+          <div>
+            <img src={student.profilePhoto} alt={`${student.names.preferredName}'s Profile`} />
+            <ul>
+              <li>Name: {student.names.preferredName} {student.names.middleName} {student.names.surname}</li>
+              <li>Email: {student.username}</li>
+              <li>Date of Birth: {student.dob}</li>
+              <li>Status: {determineTrackStatus(student)}</li>
+            </ul>
+          </div>
         </Link>
       ))}
     </section>
