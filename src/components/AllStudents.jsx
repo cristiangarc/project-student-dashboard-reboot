9f3bb7a03fd62cec8c9728e2084ba924af111f0d
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-20 12:47:13 -0500

    updated the birthday format on the AllStudents component

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -12,6 +12,33 @@ const AllStudents = ({ students, getCohort }) => {
         return isOnTrack ? "On Track" : "Off Track";
     };
 
+    const getBirthdayString = (stdnt) => {
+        const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+
+        console.log(stdnt);
+
+        const dobArr = stdnt.dob.split("/");
+        const monthIndex = dobArr[0] - 1;
+        const day = dobArr[1];
+        const year = dobArr[2];
+        const dateObj = new Date(year, monthIndex, day);
+        const month = months[dateObj.getMonth()];
+        return `${month} ${day}, ${year}`;
+    };
+
     return (
         <section className="students">
             <h2>
@@ -21,7 +48,11 @@ const AllStudents = ({ students, getCohort }) => {
             </h2>
             <h3>Total Students: {students.length}</h3>
             {students.map((student) => (
-                <Link to={`/${student.id}/student`} key={student.id}>
+                <Link
+                    to={`/${student.id}/student`}
+                    key={student.id}
+                    getBirthdayString={getBirthdayString}
+                >
                     <div>
                         <img
                             src={student.profilePhoto}
@@ -34,7 +65,7 @@ const AllStudents = ({ students, getCohort }) => {
                                 {student.names.surname}
                             </li>
                             <li>Email: {student.username}</li>
-                            <li>Date of Birth: {student.dob}</li>
+                            <li>Date of Birth: {getBirthdayString(student)}</li>
                             <li>
                                 Status: {determineTrackStatusAll(student)}
                             </li>{" "}
